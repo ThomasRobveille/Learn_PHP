7fd299ba9378458f2beebe7a61dcb4239bf12f95
Thomas Robveille <thomasrobveille@gmail.com>
2024-04-22 15:08:28 +0200

    ajout style.css, chapitre variable et condition if fait

--- Learn PHP/headers.php
+++ Learn PHP/headers.php
@@ -1,8 +1,8 @@
 <nav id="menu">        
   <div class="element_menu">
-      <h3>Titre menu</h3>
+      <h3>Accès rapide</h3>
       <ul>
-          <li><a href="page1.html">Page 1</a></li>
+          <li><a href="#variable">Les variables</a></li>
           <li><a href="page2.html">Page 2</a></li>
           <li><a href="page3.html">Page 3</a></li>
       </ul>
--- Learn PHP/index.php
+++ Learn PHP/index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cours PHP</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <h1>Page web en html pour php</h1>
@@ -12,14 +13,53 @@
     require_once(__DIR__ . '/headers.php');
   ?>
 
-  <h2>Les variables</h2>
+  <h2>Introduction</h2>
+  <p>Le PHP est un langage de programmation qui permet de créer des pages web dynamiques. Il est exécuté côté serveur et permet de générer du contenu HTML, CSS et JavaScript.</p>
+
+  <h2>Les balises PHP</h2>
+  <p>Les balises PHP permettent d'indiquer au serveur que le code qu'elles contiennent doit être interprété par le langage PHP.</p>
+  <p>Il existe trois types de balises PHP :</p>
+  <ul>
+    <li><code>&lt;?php ... ?></code> : balise d'ouverture standard</li>
+    <li><code>&lt;? ... ?></code> : balise d'ouverture courte</li>
+    <li><code>&lt;% ... %></code> : balise d'ouverture ASP</li>
+  </ul>
+
+  <h2 id="variable">Les variables</h2>
+  <p>Les variables permettent de stocker des valeurs en mémoire pour les réutiliser plus tard dans le code.</p>
+  <p>En PHP, une variable est déclarée avec le symbole <code>$</code> suivi du nom de la variable. On affecte ensuite une valeur à cette valeur via le symbole mathémmatique =.</p>
+  <p>Il existe plusieurs types de variables en PHP :</p>
+  <ul>
+    <li>Les variables de type string : <code>$nom = "Thomas";</code></li>
+    <li>Les variables de type integer : <code>$age = 18;</code></li>
+    <li>Les variables de type float : <code>$prix = 12.5;</code></li>
+    <li>Les variables de type boolean : <code>$estMajeur = true;</code></li>
+    <li>Les variables de type array : <code>$prenoms = ["Thomas", "Julie", "Pierre", "Paul"];</code></li>
+  </ul>  
+
+  <h3>Exemple</h3>
   <?php
     $nom = "Thomas";
+    $age = 18;
+    $prix = 12.5;
+    $estMajeur = true;
+    $prenoms = ["Thomas", "Julie", "Pierre", "Paul"];
 
-    echo "Bonjour $nom";
+    echo "Bonjour <b>$nom</b>, vous avez <b>$age</b> ans et le prix est de <b>$prix</b> €";
   ?>
 
+  <p>Ici dans cette phrase les mots <b>Thomas</b>, <b>18</b> et <b>12.5</b> sont des variables affichés directement dans la page web via l'instruction <b>echo</b></p>
+
   <h2>Les conditions</h2>
+  <p>Les conditions permettent d'exécuter un bloc de code si une condition est vraie.</p>
+  <p>Il existe 3 façon d'écrire des conditions en PHP</p>
+  <ul>
+    <li>Ecrire avec les mots-cles <code>if</code> et <code>else</code></li>
+    <li>Ecrire avec les mots-cles <code>switch</code> et <code>case</code></li>
+    <li>Ecrire via des opérateurs ternaires</li>
+  </ul>
+
+  <h3>Exemple</h3>
   <?php
     $age = 18;
 
@@ -30,6 +70,11 @@
     }
   ?>
 
+  <p>Avec les mots-cles if et else :</p>
+  <p><code>if(condition) { <br><pre>  echo "Rouge";</pre>} else { <br><pre>  echo "Bleu";</pre>}</code></p>
+  <p>Ici si la condition est vrai alors on affiche "Rouge" sinon on affiche "Bleu"</p>
+
+  
   <h2>Les tableaux</h2>
   <?php
     $prenoms = ["Thomas", "Julie", "Pierre", "Paul"];
